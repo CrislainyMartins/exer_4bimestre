c85eade275f5c3803616bda6957e573402a84ef0
CrislainyMartins <crislainy6339@gmail.com>
2023-11-07 10:16:15 -0300

    03

--- 4ºbimestre/lista02/03.py
+++ 4ºbimestre/lista02/03.py
@@ -1,9 +1,9 @@
 #Faça um Programa que verifique se uma letra digitada é "F" ou "M".
 #Conforme a letraescrever: F - Feminino, M - Masculino,Sexo Inválido.
 letra=input('digite seu gênero')
-if letra=="f":
+if letra=="f" or "F":
     print('gênero feminino')
-elif letra=="m":
+elif letra=="m" or "M":
     print('gênero masculino')
 else:
     print('sexo invalido')
