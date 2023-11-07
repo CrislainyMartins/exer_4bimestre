8a82c43ab52a7b76ccdd7cf41cc5b733b0672683
CrislainyMartins <crislainy6339@gmail.com>
2023-11-07 11:52:49 -0300

    10

---
+++ 4ºbimestre/lista02/10.py
@@ -0,0 +1,14 @@
+#Faça um Programa que pergunte em que turno você estuda. Peça para digitar M-matutino
+#ou V-Vespertino ou N- Noturno.
+#Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!",
+#conforme o caso
+turno=input("seu turno :")
+if turno=="m" or "matutino":
+    print("bom dia seu matuto")
+elif turno=="v" or " vesperino":
+    print('boa tarde vesperino kkkkklkk')
+elif turno== "n" or " noturno":
+    print(" boa noite coruja")
+else:
+    print("valor invalido")
+
