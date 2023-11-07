c2b76680e4f544d0b4b3a3f171d2b706930756b2
CrislainyMartins <crislainy6339@gmail.com>
2023-11-07 09:48:55 -0300

    01

---
+++ 4ºbimestre/lista02/01.py
@@ -0,0 +1,7 @@
+#Faça um Programa que peça dois números e imprima o maior deles
+n1=float(input('digite um numero '))
+n2=float(input('digite outro numero '))
+if n1>=n2:
+    print('{} é maior'.format(n1))
+else:
+    print('{} é maior'.format(n2))
