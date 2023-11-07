a16929f4decea8ead0690493f5d4796633afd9ab
CrislainyMartins <crislainy6339@gmail.com>
2023-11-07 10:09:38 -0300

    03

---
+++ 4ºbimestre/lista02/03.py
@@ -0,0 +1,11 @@
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M".
+#Conforme a letraescrever: F - Feminino, M - Masculino,Sexo Inválido.
+letra=input('digite seu gênero')
+if letra=="f":
+    print('gênero feminino')
+elif letra=="m":
+    print('gênero masculino')
+else:
+    print('sexo invalido')
+
+
