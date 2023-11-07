cb15b14e691f574b6908fcea956d8df70061c9dd
CrislainyMartins <crislainy6339@gmail.com>
2023-11-07 10:29:55 -0300

    04

--- 4ºbimestre/lista02/04.py
+++ 4ºbimestre/lista02/04.py
@@ -1,1 +1,6 @@
 #Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+letra=str(input('digite uma letra'))
+if letra=="a" or "e" or "i" or "o" or "u":
+    print('é vogal')
+else: 
+    print('é consoante')
